1f977a925c9f13b75793b76963d4ff4a2b9878c6
Mathis Hofer <hofer@puzzle.ch>
2016-03-23 15:19:01 +0100

    add layer jupa status, closes #16018

--- app/models/group/dachverband.rb
+++ app/models/group/dachverband.rb
@@ -14,6 +14,7 @@ class Group::Dachverband < Group
            Group::DachverbandVorstand,
            Group::DachverbandExterneKontakte,
            Group::DachverbandGremium,
+           Group::JugendparlamentStatus,
            Group::Jugendparlament,
            Group::Easyvote
 
