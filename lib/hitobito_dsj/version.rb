9ab1723ceb7f3da54de053a1bfc4ee24f99e7853
Pascal Zumkehr <zumkehr@puzzle.ch>
2017-06-07 11:10:47 +0200

    make version mutable again

--- lib/hitobito_dsj/version.rb
+++ lib/hitobito_dsj/version.rb
@@ -6,5 +6,5 @@
 #  https://github.com/hitobito/hitobito_dsj.
 
 module HitobitoDsj
-  VERSION = '0.0.1'.freeze
+  VERSION = '0.0.1' # rubocop:disable Style/MutableConstant or produce error when building RPM
 end
