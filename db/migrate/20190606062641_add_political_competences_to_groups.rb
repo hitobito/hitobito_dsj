984e03b2ef4da003068f6036284713a736e4a913
Matthias Viehweger <viehweger@puzzle.ch>
2023-10-11 17:51:18 +0200

    Keywordize parameters

--- db/migrate/20190606062641_add_political_competences_to_groups.rb
+++ db/migrate/20190606062641_add_political_competences_to_groups.rb
@@ -3,12 +3,12 @@ class AddPoliticalCompetencesToGroups < ActiveRecord::Migration[4.2]
     bool_defaults = { null: false, default: false }
 
     change_table :groups do |t|
-      t.boolean :competence_initiative,       bool_defaults
-      t.boolean :competence_consultation,     bool_defaults
-      t.boolean :competence_council_meeting,  bool_defaults
-      t.boolean :competence_other,            bool_defaults
-      t.boolean :competence_none,             bool_defaults
-      t.boolean :competence_unknown,          bool_defaults
+      t.boolean :competence_initiative,       **bool_defaults
+      t.boolean :competence_consultation,     **bool_defaults
+      t.boolean :competence_council_meeting,  **bool_defaults
+      t.boolean :competence_other,            **bool_defaults
+      t.boolean :competence_none,             **bool_defaults
+      t.boolean :competence_unknown,          **bool_defaults
 
       t.string :supervision_kind, null: false, default: 'unknown'
     end
